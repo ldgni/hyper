e4c85d5e748ec87b6a6a708c9f77569a83fdc07d
Luca Di Gianni <luca.dgni@gmail.com>
2025-08-11 14:56:31 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Repository name
+
+> Description
+
+## Running Locally
+
+1. Clone the repository
+
+```sh
+git clone https://github.com/ldgni/repo-name.io
+```
+
+2. Navigate inside the repository
+
+```sh
+cd repo-name
+```
+
+3. Install dependencies
+
+```sh
+npm install
+```
+
+4.  Start the development server
+
+```sh
+npm run dev
+```
+
+## Contributing
+
+Pull requests are appreciated. For major changes, please open an issue first.
+
+## License
+
+This project is licensed under the terms of the [MIT License](LICENSE).
---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import checkFile from "eslint-plugin-check-file";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "check-file": checkFile,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "**/": "NEXT_JS_APP_ROUTER_CASE",
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
