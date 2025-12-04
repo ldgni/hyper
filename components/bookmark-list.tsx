2d2320d9c6f58b7c52a9cb492c386cca7179f305
Luca Di Gianni <luca.dgni@gmail.com>
2025-12-04 12:05:31 +0100

    fix: layout padding

--- components/bookmark-list.tsx
+++ components/bookmark-list.tsx
@@ -25,7 +25,7 @@ interface BookmarkListProps {
 
 export default function BookmarkList({ bookmarks }: BookmarkListProps) {
   return (
-    <>
+    <div className="px-3">
       <div className="mb-8 flex items-center justify-between">
         <h1 className="text-2xl font-bold">Bookmarks</h1>
         <AddBookmarkDialog />
@@ -63,6 +63,6 @@ export default function BookmarkList({ bookmarks }: BookmarkListProps) {
           ))}
         </div>
       )}
-    </>
+    </div>
   );
 }
